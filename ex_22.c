811317c9d10c0457be7b0f469ae58a4f5c6e4629
kimchaeyoung_student <chaeyoung0584@gmail.com>
2019-07-23 19:25:21 +0900

    C practice_file

---
+++ ex_22.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+main(){
+	char ch;
+	printf("덧셈진행(y/n) : ");
+	scanf("%c", &ch);
+	
+	if(ch=='y')
+		quiz();
+	else
+		printf("수고하셨습니다.");
+}
+
+quiz(){
+	int a, b, answer, t=1;
+	char ch;
+	printf("두 수를 입력해주세요 : ");
+	fflush(stdin); //메모리를 비워준다 
+	scanf("%d %d", &a, &b);
+	
+	re_answer:
+	printf("두 수의 합 : ");
+	scanf("%d", &answer);
+	
+	if(answer==(a+b)){
+		printf("정답! \n");
+	}
+	else{
+		printf("오답! \n");
+		t=t+1; //오답 횟수 구하기, t 초기화 필수
+		goto re_answer;
+	}
+	printf("정답률 : %.0f%% \n", 1/(float)t*100); //%d==%.0f
+}
+

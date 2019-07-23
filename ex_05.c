d3ba8ca403fefe0d2469f1469981f7ff01c29f05
kimchaeyoung_student <chaeyoung0584@gmail.com>
2019-07-23 19:09:02 +0900

    C practice_file

---
+++ ex_05.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	float a = 10, b = 20;
+	double percent = a / 100;
+	double c = 0.3;
+	
+	printf("percent is %f . \n", percent);
+	printf("percent is %e . \n", percent);
+	printf("percent is %a . \n", percent);
+	printf("a : %f, b : %e", a, b);
+	
+}
+

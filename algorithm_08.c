40a51e476fd701aa526225ab7680801e767fd30b
kimchaeyoung_student <chaeyoung0584@gmail.com>
2019-07-30 21:57:31 +0900

    190730_practice

---
+++ algorithm_08.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main(){
+	int ARR_MAX = 4;
+	int a[] = {8,0,7,1};
+	int b[] = {10,3,0,1};
+	int c[3];
+	int i;
+	
+	for(i=0;i<ARR_MAX;i++){
+		c[i] = a[i] + b[i];
+	}
+	for(i=0;i<ARR_MAX;i++){
+		printf("%dx^%d + ", c[i], (ARR_MAX-i-1));
+	}
+	printf("%d ", c[ARR_MAX-1]);
+}

46438022f7189267f326a5d00dd6b3031d8e4e05
kimchaeyoung_student <chaeyoung0584@gmail.com>
2019-07-30 21:55:39 +0900

    190730_practice

---
+++ algorithm_02.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+void main(void){
+	clock_t start, finish;
+	double duration;
+	start = clock();
+	
+	finish = clock();
+	duration = (double)(finish-start) / CLOCKS_PER_SEC;
+	printf("%f초 입니다.\n", duration);
+}

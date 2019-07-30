76e22f7ee7ff72c28347a50922063f98ecdcf370
kimchaeyoung_student <chaeyoung0584@gmail.com>
2019-07-30 21:56:03 +0900

    190730_practice

---
+++ algorithm_03.c
@@ -0,0 +1,16 @@
+//조합값을 구하는 프로그래밍 (이항계수) 
+#include<stdio.h>
+main(){
+	int n=5, k=3;
+	int dp[100][100];
+	int i,j;
+	
+	//scanf("%d %d", &n, &k);
+	for(i=1;i<=n;i++){
+		dp[i][0] = dp[i][i] = 1;
+		for(j=1;j<i;j++){
+			dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+		}
+	}
+	printf("%d\n", dp[n][k]);	//5C3 = 10;(?)
+}
